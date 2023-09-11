29bd5f791ba3266eddebf878417946cf2f0a4fb3
Matheus do Nascimento Murucci Ferreira <VIC0131800@aluno.firjansenaisesi.com.br>
2023-09-11 12:08:32 -0300

    Avanços no java script

--- JS/Cliente.js
+++ JS/Cliente.js
@@ -1,31 +1,25 @@
-const enderecoForm = document.querySelector(".form-body");
-const cepInput = document.querySelector("#cep");
-const ruaInput = document.querySelector("#rua");
-const bairroInput = document.querySelector("#bairro");
-const cidadeInput = document.querySelector("#cidade");
-const estadoInput = document.querySelector("#estado");
-
-cepInput.addEventListener("keypress", (e) => {
-    const onlyNumbers = /[0-9]/;
-    const tecla = String.fromCharCode(e.keyCode)
-    if(!onlyNumbers.test(tecla)){
-        e.preventDefault();
-        return;
-    }
-});
+var enderecoForm = document.querySelector(".form-body");
+var cepInput = document.querySelector("#cep");
+var ruaInput = document.querySelector("#rua");
+var bairroInput = document.querySelector("#bairro");
+var cidadeInput = document.querySelector("#cidade");
+var estadoInput = document.querySelector("#estado");
+var formInput = document.querySelector("[data-input]");
 
 cepInput.addEventListener("keyup", (e) => {
-
-    const inputValue = e.target.value;
-    if (inputValue.lenght === 8){
-        getEndereco(inputValue);
-    }
+    const inputValue = e.target.value;   
+    getEndereco(inputValue);
 });
 
-const getEndereco = async (cep) => {
-
+var getEndereco = async (cep) => {
     const apiURL = `https://viacep.com.br/ws/${cep}/json/`;
     const response = await fetch(apiURL);
     const data = await response.json();
     console.log(data);
+    console.log(formInput);
+    console.log(data.erro);
+    ruaInput.value = data.logradouro;
+    cidadeInput.value = data.localidade;
+    bairroInput.value = data.bairro;
+    estadoInput.value = data.uf;
 }
